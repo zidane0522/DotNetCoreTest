3a1102e8e9ce66482065a300f8d471afaab09600
yuanying <lishen@xizhaoip.com>
2018-11-22 20:56:51 +0800

    alter

--- OcelotWebGateApi/IdentityService/IdentityConfig.cs
+++ OcelotWebGateApi/IdentityService/IdentityConfig.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using IdentityServer4.Models;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,7 +10,28 @@ namespace IdentityService
     public class IdentityConfig
     {
 
+        public static IConfiguration Configuration { get; set; }
 
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("api1","ApiServerOne"),
+                new ApiResource("api2","ApiServerTwo")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client> {
+                new Client(){
+                    ClientId="client",
+                    ClientName="clientName",
+                    ClientSecrets={ new Secret("one".Sha256()),new Secret("two".Sha256())},
+                    AllowedScopes={ "api1","api2"}
+                }
+            };
+        }
     }
 
 
---
+++ OcelotWebGateApi/IdentityService/ResourceOwnerPasswordValidator.cs
@@ -0,0 +1,18 @@
+﻿using IdentityServer4.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityService
+{
+    public class ResourceOwnerPasswordValidator : IResourceOwnerPasswordValidator
+    {
+        //private  loginUserService;
+
+        public Task ValidateAsync(ResourceOwnerPasswordValidationContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
