6863553c56a4f0b58aad869d3256814a57d5287e
zidanepc <zidanepc@DESKTOP-VIIUN1E>
2018-11-23 00:01:32 +0800

    alter

--- OcelotWebGateApi/IdentityService/ResourceOwnerPasswordValidator.cs
+++ OcelotWebGateApi/IdentityService/ResourceOwnerPasswordValidator.cs
@@ -1,18 +1,59 @@
-﻿using IdentityServer4.Validation;
+﻿using IdentityServer4.Models;
+using IdentityServer4.Validation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace IdentityService
 {
     public class ResourceOwnerPasswordValidator : IResourceOwnerPasswordValidator
     {
-        //private  loginUserService;
+        private  ILoginUserService loginUserService;
+
+        public ResourceOwnerPasswordValidator(ILoginUserService _loginUserService)
+        {
+            this.loginUserService = _loginUserService; 
+        }
 
         public Task ValidateAsync(ResourceOwnerPasswordValidationContext context)
         {
-            throw new NotImplementedException();
+            LoginUser loginUser = null;
+            bool isAuthenticated = loginUserService.Authenticate(context.UserName,context.Password,out loginUser);
+            if (!isAuthenticated)
+            {
+                context.Result = new GrantValidationResult(TokenRequestErrors.InvalidGrant,"Invaild client credentials");
+            }
+            else
+            {
+                context.Result = new GrantValidationResult(
+                    subject: context.UserName,
+                    authenticationMethod: "custom",
+                    claims: new Claim[] {
+                        new Claim("Name",context.UserName),
+                        new Claim("Id",loginUser.Id.ToString()),
+                        new Claim("RealName", loginUser.RealName),
+                        new Claim("Email", loginUser.Email)
+                    }
+                    );
+            }
+
+            return Task.CompletedTask;
         }
+    }
+
+    public class LoginUser
+    {
+        public int Id { get; set; }
+
+        public string RealName { get; set; }
+
+        public string Email { get; set; }
+    }
+
+    public interface ILoginUserService
+    {
+        bool Authenticate(string loginName,string password,out LoginUser loginUser);
     }
 }
